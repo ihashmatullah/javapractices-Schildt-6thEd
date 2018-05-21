ed8531cb7ea5b78393b7a6750af871b649828d09
iHashmatullah <ihashmatullah@github.com>
2018-05-21 16:18:38 -0500

    6 practice programs added

---
+++ GalToLit.java
@@ -0,0 +1,12 @@
+class GalToLit {
+	public static void main(String args[]) {
+		double gallons;
+		double liters;
+
+		gallons = 10;
+
+		liters = 3.7854 * gallons;
+
+		System.out.println(gallons + " gallons to liter is: " + liters);
+	}
+}
